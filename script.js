3025f39e81fb1734b373db2c4e835541f1a49388
elycava <elycavaleiro@gmail.com>
2026-06-14 13:03:27 -0300

    Primer versión Prode Mundial

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
 document.getElementById("app").innerHTML = `
 <div class="app">
+
   <header class="main-header">
-    <h1>🏆 FIFA WORLD CUP 2026</h1>
-    <p>Prode Mundial 2026</p>
+    <div>
+      <h1>🏆 FIFA WORLD CUP 2026</h1>
+      <p>Prode Mundial 2026 • Hora Argentina</p>
+    </div>
   </header>
 
   <div class="match-card">
-    <h2>Argentina vs Argelia</h2>
-    <input type="number" class="input-goals" placeholder="ARG">
-    <input type="number" class="input-goals" placeholder="ALG">
+
+    <div class="match-info">
+      <span>📅 16/06/2026 - 22:00 hs</span>
+      <span>Grupo J</span>
+    </div>
+
+    <div class="match-teams">
+
+      <div class="team">
+        🇦🇷 Argentina
+      </div>
+
+      <div>
+        <input class="input-goals" type="number">
+        vs
+        <input class="input-goals" type="number">
+      </div>
+
+      <div class="team">
+        Argelia 🇩🇿
+      </div>
+
+    </div>
+
   </div>
+
 </div>
 `;
