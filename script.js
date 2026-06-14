edc18e91e7f5520827541e185bf16b158d9dae26
elycava <elycavaleiro@gmail.com>
2026-06-14 12:03:28 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("app").innerHTML = `
+<div class="app">
+  <header class="main-header">
+    <h1>🏆 FIFA WORLD CUP 2026</h1>
+    <p>Prode Mundial 2026</p>
+  </header>
+
+  <div class="match-card">
+    <h2>Argentina vs Argelia</h2>
+    <input type="number" class="input-goals" placeholder="ARG">
+    <input type="number" class="input-goals" placeholder="ALG">
+  </div>
+</div>
+`;
